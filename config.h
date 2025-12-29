43c0fff0c0100fb705d0b2e601cd99a9133c03b5
amitshky <amitshkymail@gmail.com>
2025-12-29 17:44:24 +0000

    alpha patch applied

--- config.h
+++ config.h
@@ -9,6 +9,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMonoNLNerdFont:size=12" };
 static const char dmenufont[]       = "JetBrainsMonoNLNerdFont:size=12";
+static unsigned int baralpha        = 0xd0;
+static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
