467ddd9b2d2e66ffb45d884de0d9a00059a31609
amitshky <amitshkymail@gmail.com>
2026-01-02 14:00:22 +0000

    statusbar color changes

--- config.h
+++ config.h
@@ -9,18 +9,18 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMonoNLNerdFont:size=12" };
 static const char dmenufont[]       = "JetBrainsMonoNLNerdFont:size=12";
-static unsigned int baralpha        = 0xFF;
+static unsigned int baralpha        = OPAQUE;
 static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#323232";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_red[]         = "#ff8282";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_red  },
+	[SchemeSel]  = { col_gray4, col_gray1, col_red  },
 };
 
 /* tagging */
@@ -71,21 +71,24 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-l", "5", "-i", NULL };
 static const char *termcmd[]  = { "st", NULL };
-// quick launch
+/* quick launch */
 static const char *filemancmd[] = { "pcmanfm", NULL };
 static const char *browsercmd[] = { "firefox", NULL };
 
-// tools
+/* tools */
 static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 static const char *clipboardcmd[] = { "copyq", "show", NULL };
 static const char *colorpickerwincmd[] = { "gpick", NULL };
 static const char *colorpickercmd[] = { "gpick", "-p", NULL };
 
-// applications
+/* applications */
 /*static const char *soundmixercmd[] = { "st", "-e", "pulsemixer", NULL };*/
 static const char *calendarcmd[] = { "st", "-e", "calcure", NULL };
 static const char *processmgrcmd[] = { "st", "-e", "btop", NULL };
+
+/* scripts */
 static const char *powermenucmd[] = { "/usr/bin/env", "bash", "-c", "$HOME/dev/config/scripts/linux/powermenu", NULL };
+static const char *cleanupcmd[] = { "/usr/bin/env", "bash", "-c", "$HOME/dev/config/scripts/linux/cleanup.sh", NULL };
 
 /* audio controls */
 static const char *volupcmd[]   = { "/usr/bin/env", "bash", "-c", "pactl set-sink-volume 0 +5% && pkill -RTMIN+10 dwmblocks", NULL };
@@ -100,7 +103,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_o,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_z,      zoom,           {0} },
@@ -130,17 +133,18 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 
-	/* custom controls */
+	/* custom keymaps */
 	{ MODKEY,                       XK_a,          spawn,      {.v = filemancmd } },
 	{ MODKEY,                       XK_s,          spawn,      {.v = browsercmd } },
 	{ 0,                            PRINTSCREEN,   spawn,      {.v = screenshotcmd } },
 	{ MODKEY|ShiftMask,             XK_s,          spawn,      {.v = screenshotcmd } },
 	{ MODKEY,                       XK_v,          spawn,      {.v = clipboardcmd } },
 	{ ALTKEY,                       XK_v,          spawn,      {.v = clipboardcmd } },
-	{ MODKEY|ShiftMask,             XK_c,          spawn,      {.v = colorpickerwincmd } },
-	{ MODKEY|ShiftMask,             XK_p,          spawn,      {.v = colorpickercmd } },
+	{ MODKEY|ShiftMask|ALTKEY,      XK_c,          spawn,      {.v = colorpickerwincmd } },
+	{ MODKEY|ShiftMask,             XK_c,          spawn,      {.v = colorpickercmd } },
 	{ MODKEY,                       XK_c,          spawn,      {.v = calendarcmd } },
 	{ MODKEY,                       XK_Escape,     spawn,      {.v = processmgrcmd } },
+	{ MODKEY|ALTKEY,                XK_c,          spawn,      {.v = cleanupcmd } },
 
 	/* audio controls */
 	{ 0,                  XF86XK_AudioRaiseVolume, spawn,      {.v = volupcmd } },
