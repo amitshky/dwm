06f131b993148f195c7620e6fabc0a61d4258f44
amitshky <amitshkymail@gmail.com>
2026-01-02 14:16:39 +0000

    status bar colors

--- config.h
+++ config.h
@@ -13,14 +13,14 @@ static unsigned int baralpha        = OPAQUE;
 static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#323232";
+static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_red[]         = "#ff8282";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1, col_red  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_red  },
 };
 
 /* tagging */
